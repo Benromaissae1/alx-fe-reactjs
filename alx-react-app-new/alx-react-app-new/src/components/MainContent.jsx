479b4a6eb31d2da49623fab615a451fe37dc7d39
benromaissae1 <benromaissae1@gmail.com>
2025-09-08 00:11:00 +0200

    react

---
+++ alx-react-app-new/alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,21 @@
+
+function MainContent() {
+    return (
+      <main
+        style={{
+          padding: "20px",
+          backgroundColor: "#eef2f3",
+          minHeight: "200px",
+        }}
+      >
+        <h2 style={{ textAlign: "center", color: "darkred" }}>
+          Explore the World
+        </h2>
+        <p style={{ textAlign: "center", fontSize: "18px" }}>
+          I love to visit New York, Paris, and Tokyo.
+        </p>
+      </main>
+    );
+  }
+  
+  export default MainContent;
